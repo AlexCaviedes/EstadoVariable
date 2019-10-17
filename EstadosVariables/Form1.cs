a44bd69338055960aa6d33fa3e86231ea0fec0ac
Usuario <100_Jornada1@FormacionSena.red>
2019-10-17 16:10:09 -0500

    variables

---
+++ EstadosVariables/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstadosVariables
+{
+    public partial class Form1 : Form
+    {
+        private int Uno;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIncrementar_Click(object sender, EventArgs e)
+        {
+            Uno++;
+            txtNumero.Text = Uno.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNumero.Text = "1";
+        }
+    }
+}
